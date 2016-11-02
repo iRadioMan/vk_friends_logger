6003d7bb3b962023178917c53201970500cc5d23
RadioMan <ilyadud@mail.ru>
2016-11-02 10:07:00 +0700

    Now checking for existing of account.txt

--- vk_config.py
+++ vk_config.py
@@ -2,10 +2,17 @@
 c_delay = 10
 c_oldfriends = 'oldfriends.txt'
 
-f = open('account.txt', 'r')
-user_login = f.readline()
-user_password = f.readline()
-f.close()
+import os
+if not os.path.isfile('account.txt'):
+	f = open('account.txt', 'w')
+	f.write('\n')
+	user_login = '\n'
+	f.close()
+else:
+	f = open('account.txt', 'r')
+	user_login = f.readline()
+	user_password = f.readline()
+	f.close()
 
 def acc_save(new_pass):
 	f = open('account.txt', 'w')
